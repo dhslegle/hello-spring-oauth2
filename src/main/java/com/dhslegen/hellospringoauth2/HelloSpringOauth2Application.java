36cd2d792e4405d9ea3d7a15dc1055824f95a553
zhaowenhao <37139487+dhslegen@users.noreply.github.com>
2024-02-28 14:35:03 +0800

    first commit

---
+++ src/main/java/com/dhslegen/hellospringoauth2/HelloSpringOauth2Application.java
@@ -0,0 +1,13 @@
+package com.dhslegen.hellospringoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhslegen/hellospringoauth2/HelloSpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhslegen.hellospringoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
